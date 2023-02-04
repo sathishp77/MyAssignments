7b990ebec05df0c168dc89181145a8307f23faa3
sathishp77 <124438930+sathishp77@users.noreply.github.com>
2023-02-04 17:29:52 +0530

    Add files via upload
    Week1_Day1_Assignment

---
+++ week1/day1Assignment1/Mobile.java
@@ -0,0 +1,57 @@
+package week1.day1Assignment1;
+
+public class Mobile {
+public static void main(String[] args) {
+	String mobileBrandName = "OnePlus";
+	System.out.println(mobileBrandName);
+	char mobileLogo= '+';
+	System.out.println(mobileLogo);
+	short noOfMobilePiece = 100;
+	System.out.println(noOfMobilePiece);
+	int modelNumber = 765432;
+	System.out.println(modelNumber);
+	long mobileImeiNumber = 5593214678901234567L;
+	System.out.println(mobileImeiNumber);
+	float mobilePrice = 41449.79F;
+	System.out.println(mobilePrice);
+	boolean isDamaged= true;
+	System.out.println(isDamaged);
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+	/*
+	String mobileBrandName = "OnePlus";
+			System.out.println("Mobile Brand Name: "+mobileBrandName);
+			char mobileLogo= '+';
+			System.out.println("Mobile Logo: "+mobileLogo);
+			short noOfMobilePiece = 100;
+			System.out.println("No of Mobile Piece: "+noOfMobilePiece);
+			int modelNumber = 765432;
+			System.out.println("Mobile Number: "+modelNumber);
+			long mobileImeiNumber = 5593214678901234567L;
+			System.out.println("Mobile IMEI Number: "+mobileImeiNumber);
+			float mobilePrice = 41449.79F;
+			System.out.println("Mobile Price: "+mobilePrice);
+			boolean isDamaged= true;
+			System.out.println("Is Mobile Damaged: "+isDamaged);
+			*/
+	}
+}
---
+++ week1/day1Assignment1/TwoWheeler.java
@@ -0,0 +1,36 @@
+package week1.day1Assignment1;
+
+public class TwoWheeler {
+	public static void main(String[] args) {
+		int noOfWheels = 2;
+		System.out.println(noOfWheels);
+		short noOfMirrors = 2;
+		System.out.println(noOfMirrors);
+		long chassisNumber = 1122334455667788L;
+		System.out.println(chassisNumber);
+		boolean isPunctured = false;
+		System.out.println(isPunctured);
+		String bikeName = "Pulsar";
+		System.out.println(bikeName);
+		double runningKM = 15012.9654;
+		System.out.println(runningKM);
+
+		
+		
+		/*
+		int noOfWheels = 2;
+		System.out.println("No of Wheels: "+noOfWheels);
+		short noOfMirrors = 2;
+		System.out.println("No of Mirrors: "+noOfMirrors);
+		long chassisNumber = 1122334455667788L;
+		System.out.println("Chassis Number: "+chassisNumber);
+		boolean isPunctured = false;
+		System.out.println("Is bike punctured: "+isPunctured);
+		String bikeName = "Pulsar";
+		System.out.println("Name of the bike: "+bikeName);
+		double runningKM = 15012.9654;
+		System.out.println("No of KM running: "+runningKM);
+*/
+		
+	}
+}
